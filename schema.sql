53fe63585e37dd2e8c6b4ed1283d6e4b7fd3d79f
IvanMukherjee <120264413+IvanMukherjee@users.noreply.github.com>
2026-04-28 17:45:05 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,7 +5,10 @@ USE fraud_detection;
 -- Users table
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
-    name VARCHAR(100)
+    name VARCHAR(100),
+    account_created DATE,
+    kyc_status VARCHAR(20),
+    country VARCHAR(50)
 );
 
 -- Transactions table
@@ -15,13 +18,16 @@ CREATE TABLE transactions (
     txn_amount DECIMAL(10,2),
     txn_type VARCHAR(10),
     txn_time DATETIME
+    merchant VARCHAR(50),
+    location VARCHAR(50)
 );
 
 -- Logins table
 CREATE TABLE logins (
     login_id INT PRIMARY KEY,
     user_id INT,
-    location VARCHAR(50),
+    login_time DATETIME,
+    IP_address VARCHAR(50),
     device VARCHAR(50),
-    login_time DATETIME
+    location VARCHAR(50)
 );
